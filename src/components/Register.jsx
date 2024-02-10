4066fa6cc74c7fc727ad116243f51d0da30e00ea
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-02-10 23:26:59 +0500

    regiter template is ready

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -16,11 +16,55 @@ const Register = () => {
                     </label>
                     <input type="text" className="form-control" />
                   </div>
+                </div>
+                <div className="col-lg-6">
                   <div className="form-group">
                     <label>
                       Password <span className="errmsg">*</span>
                     </label>
                     <input type="password" className="form-control" />
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="form-group">
+                    <label>
+                      Full Name <span className="errmsg">*</span>
+                    </label>
+                    <input type="text" className="form-control" />
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="form-group">
+                    <label>
+                      Email <span className="errmsg">*</span>
+                    </label>
+                    <input type="email" className="form-control" />
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="form-group">
+                    <label>
+                      Phone <span className="errmsg">*</span>
+                    </label>
+                    <input type="tel" className="form-control" />
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="form-group">
+                    <label>
+                      Country <span className="errmsg">*</span>
+                    </label>
+                    <select name="" id="" className="form-control">
+                      <option value="uzbekistan">Uzbekistan</option>
+                      <option value="saudiarabia">Saudi Arabia</option>
+                      <option value="palestine">Palestine</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="col-lg-12">
+                  <div className="form-group">
+                    <label>Address</label>
+                    <textarea className="form-control"></textarea>
                   </div>
                 </div>
               </div>
