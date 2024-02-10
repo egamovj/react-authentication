59537777c24019bae7a3a995c6e89a8e160865f0
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-02-10 23:15:50 +0500

    added user name and password

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,24 +1,40 @@
-
 const Register = () => {
   return (
     <div>
-        <div className="offset-lg-3 col-lg-6">
-            <form className="container">
-                <div className="card">
-                    <div className="card-header">
-                        <h1>User Registration</h1>
-                    </div>
-                    <div className="card-body">
-
-                    </div>
-                    <div className="card-footer">
-                        <button type="submit" className="btn btn-primary">Register</button>
-                    </div>
+      <div className="offset-lg-3 col-lg-6">
+        <form className="container">
+          <div className="card">
+            <div className="card-header">
+              <h1>User Registration</h1>
+            </div>
+            <div className="card-body">
+              <div className="row">
+                <div className="col-lg-6">
+                  <div className="form-group">
+                    <label>
+                      User Name <span className="errmsg">*</span>
+                    </label>
+                    <input type="text" className="form-control" />
+                  </div>
+                  <div className="form-group">
+                    <label>
+                      Password <span className="errmsg">*</span>
+                    </label>
+                    <input type="password" className="form-control" />
+                  </div>
                 </div>
-            </form>
-        </div>
+              </div>
+            </div>
+            <div className="card-footer">
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
