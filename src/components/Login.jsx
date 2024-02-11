79d36f374f81750071b0b8e9f775b03c83c5361d
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-02-11 10:34:17 +0500

    login pgae

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,34 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const ProceedLogin = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="row">
+      <div className="offset-lg-3 col-lg-6">
+        <form className="container" onSubmit={ProceedLogin}>
+          <div className="card">
+            <div className="card-header">
+              <h2>User Login</h2>
+            </div>
+            <div className="card-body">
+              <div className="form-group">
+                <label>User Name <span>*</span></label>
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+            <div className="card-footer"></div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
